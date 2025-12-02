4d92070c59b5a7c6943be42a2c9cb8ed357f49e0
Andres <118140778+AndresMagl2025@users.noreply.github.com>
2025-12-02 20:34:55 -0300

    Update operarios.js

--- js/operarios.js
+++ js/operarios.js
@@ -1,1 +1,13 @@
+const OPERARIOS = {
+  "FORMICONI": "Germán Formiconi",
+  "BARCO": "Gustavo Barco",
+  "MAINARDI": "Esteban Mainardi",
+  "GODOY": "José Luis Godoy",
+  "MAGLIANO": "Andrés Magliano",
+  "LIEVY": "Gustavo Lievy",
+  "PERALTA": "Mauro Peralta",
+  "CABALLERO": "Víctor Caballero",
+  "PESCE": "David Pesce"
+  // ... agregá los demás según tu lista real
+};
 
